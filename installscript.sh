8cde757660f11191b09d36550a83f9a6ad975a5c
Nico Brandt <st115493@stud.uni-stuttgart.de>
2020-10-05 22:38:18 +0200

    [add] provisioning files

---
+++ installscript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "installing InfluxDB"
+sudo apt-get update && sudo apt-get install influxdb
+sudo systemctl unmask influxdb.service
+echo "influxdb -version"
+sudo systemctl start influxdb
+echo "installing Grafana"
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/enterprise/deb stable-main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "copying provision files"
+sudo mv /provisioning/* /etc/grafana/provisioning
+echo "starting grafana server"
+sudo systemctl deamon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+echo "configuring grafana-server to start at boot"
+sudo systemctl enable grafana-server.service
+/usr/bin/firefox&  localhost:3000
