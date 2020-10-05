f68a955866c5e8767e6733da052f1777837dd6cd
Nico Brandt <st115493@stud.uni-stuttgart.de>
2020-10-06 00:13:57 +0200

    [fix] grafana installation

--- installscript.sh
+++ installscript.sh
@@ -9,7 +9,7 @@ echo "installing Grafana"
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/enterprise/deb stable-main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
 sudo apt-get install grafana
 echo "copying provision files"
