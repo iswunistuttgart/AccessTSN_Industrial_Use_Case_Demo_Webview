622fda432191f39b714b137c2ae8d34a218ac235
Nico Brandt <st115493@stud.uni-stuttgart.de>
2020-10-06 01:07:14 +0200

    [fix] cp provisioning into correct directory

--- installscript.sh
+++ installscript.sh
@@ -10,16 +10,16 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
+sudo apt-get update/
 sudo apt-get install grafana
 echo "copying provision files"
-sudo mkdir -p /etc/grafana/provisioning
-sudo cp -r ./provisioning/* /etc/grafana/provisioning
+#sudo mkdir -p /etc/grafana
+sudo cp -r provisioning/ /etc/grafana/
 echo "configuring grafana-server to start at boot"
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server.service
 echo "configuring grafana-server to start at boot"
 sudo systemctl start grafana-server
-gnome-terminal -e "systemctl status grafana-server"
+gnome-terminal -- "systemctl status grafana-server"
 echo "configuring grafana-server to start at boot"
-/usr/bin/firefox&  localhost:3000
+xdg-open http://localhost:3000
