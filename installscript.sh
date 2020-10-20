864a1da7560a11e934b573bcc073872803aeee5f
Nico Brandt <st115493@stud.uni-stuttgart.de>
2020-10-20 15:39:04 +0200

    [fix] grafana version and deleted sudo's

--- installscript.sh
+++ installscript.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 echo "installing InfluxDB"
-sudo apt-get update && sudo apt-get install influxdb
-sudo systemctl unmask influxdb.service
+apt-get update && sudo apt-get install influxdb
+systemctl unmask influxdb.service
 echo "influxdb -version"
-sudo systemctl start influxdb
+systemctl start influxdb
 echo "installing Grafana"
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
+apt-get install -y apt-transport-https
+apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install grafana
 echo "copying provision files"
-sudo cp -r provisioning/ /etc/grafana/
-sudo chgrp -R grafana /etc/grafana/provisioning/
+cp -r provisioning/ /etc/grafana/
+chgrp -R grafana /etc/grafana/provisioning/
 echo "configuring grafana-server to start at boot"
-sudo systemctl daemon-reload
-sudo systemctl enable grafana-server.service
-sudo systemctl start grafana-server
+systemctl daemon-reload
+systemctl enable grafana-server.service
+systemctl start grafana-server
