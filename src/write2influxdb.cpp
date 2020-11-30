78ec8bb59c60b539ae2b8686f307d523e0d4d03c
Philipp Neher <philipp.neher@isw.uni-stuttgart.de>
2020-11-30 19:26:59 +0100

    update Readme, Makefile, inlcudes, and common submodule

--- src/write2influxdb.cpp
+++ src/write2influxdb.cpp
@@ -24,8 +24,7 @@
  */
 
 #include "../influxdb-cpp/influxdb.hpp"
-#include "../accesstsn_demoapps_common/mk_shminterface.h"
-#include "mk_shminterface.h"
+#include "demoapps_common/mk_shminterface.h"
 #include <signal.h>
 #include <sys/mman.h>
 #include <fcntl.h>
